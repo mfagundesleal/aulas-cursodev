cd7ec1262abd7d4e2b24f088f61dd3a0d09e97a9
Michel <48397098+mfagundesleal@users.noreply.github.com>
2025-01-21 01:02:04 +0000

    adiciona endpoint `/api/v1/status`

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "67b61d27953143ad"; }
+/******/ 		__webpack_require__.h = function() { return "00996caa80d1c507"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
